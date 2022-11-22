90cb41cc8a0c0dce54475a982d1fe58c3c46dfaf
Rifkhankhan <you@example.comclscls>
2022-11-22 14:04:00 +0530

    created some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './Staff/teacher/teacher.component';
+import { ManagementComponent } from './Staff/management/management.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent,
+    StudentComponent,
+    TeacherComponent,
+    ManagementComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
